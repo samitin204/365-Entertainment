459c193092bb4c8aa0b6d93d5fb2b63ad9cf2400
samitin204 <87005449+samitin204@users.noreply.github.com>
2021-07-06 14:33:25 -0400

    Add files via upload

---
+++ 365 Entertainment - English/index.js
@@ -0,0 +1,160 @@
+var data = '[{"divid":"Drama","divclass":"character box drama all textOverImage","divname":"23 walks", "divdatatext":"23 walks","imageclass": "genre_image","imageid":"img1","imagepath":"Movie_images/All/Drama/23 walks.jpg"},{"divid":"Drama","divclass":"character box drama all textOverImage","divname":"Dutch kills", "divdatatext":"Dutch kills","imageclass": "genre_image","imageid":"img2","imagepath":"Movie_images/All/Drama/Dutch kills.jpg"},{"divid":"Drama","divclass":"character box drama all textOverImage","divname":"Invisible Man", "divdatatext":"Invisible Man bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img3","imagepath":"Movie_images/All/Drama/Invisible Man.jpg"},{"divid":"Drama","divclass":"character box drama all textOverImage","divname":"The Woman in window", "divdatatext":"The Woman in window bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img4","imagepath":"Movie_images/All/Drama/The Woman in window.jpg"},{"divid":"Drama","divclass":"character box drama all textOverImage","divname":"Long Weekend", "divdatatext":"Long Weekend bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img5","imagepath":"Movie_images/All/Drama/Long Weekend.jpg"},{"divid":"Drama","divclass":"character box drama all textOverImage","divname":"Little-Women", "divdatatext":"Little-Women bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img6","imagepath":"Movie_images/All/Drama/Little-Women.jpg"},{"divid":"Thriller","divclass":"character box thriller all textOverImage","divname":"All the kind strangers", "divdatatext":"All the kind strangers bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img7","imagepath":"Movie_images/All/Thriller/All the kind strangers.jpg"},{"divid":"Thriller","divclass":"character box thriller all textOverImage","divname":"Honeymoon", "divdatatext":"Honeymoon bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img8","imagepath":"Movie_images/All/Thriller/Honeymoon.jpg"},{"divid":"Thriller","divclass":"character box thriller all textOverImage","divname":"Under the bed", "divdatatext":"Under the bed bisubfiuebfiurbifbirbgirbgierbgir","imageclass":"genre_image","imageid":"img9","imagepath":"Movie_images/All/Thriller/Under the bed.jpg"},{"divid":"Thriller","divclass":"character box thriller all textOverImage","divname":"Digging-up-the-marrow-movie", "divdatatext":"Digging-up-the-marrow-movie bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img10","imagepath":"Movie_images/All/Thriller/Digging-up-the-marrow-movie.jpg"},{"divid":"Thriller","divclass":"character box thriller all textOverImage","divname":"Hebecta", "divdatatext":"Hebecta bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img11","imagepath":"Movie_images/All/Thriller/Hebecta.jpg"},{"divid":"Thriller","divclass":"character box thriller all textOverImage","divname":"Thriller", "divdatatext":"Thriller bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img12","imagepath":"Movie_images/All/Thriller/Thriller.jpg"},{"divid":"Horror","divclass":"character box horror all textOverImage","divname":"Across the river", "divdatatext":"Across the river bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img13","imagepath":"Movie_images/All/Horror/Across the river.jpg"},{"divid":"Horror","divclass":"character box horror all textOverImage","divname":"Curse of the witch doll", "divdatatext":"Curse of the witch doll bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img14","imagepath":"Movie_images/All/Horror/Curse of the witch doll.jpg"},{"divid":"Horror","divclass":"character box horror all textOverImage","divname":"Drag me to Hell", "divdatatext":"Drag me to Hell bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img15","imagepath":"Movie_images/All/Horror/Drag me to Hell.jfif"},{"divid":"Horror","divclass":"character box horror all textOverImage","divname":"Werewolf poster werewolf the beast among us", "divdatatext":"Werewolf poster werewolf the beast among us bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img16","imagepath":"Movie_images/All/Horror/Werewolf poster werewolf the beast among us.jpg"},{"divid":"Horror","divclass":"character box horror all textOverImage","divname":"Horror", "divdatatext":"Horror bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img17","imagepath":"Movie_images/All/Horror/Horror.jpg"},{"divid":"Horror","divclass":"character box horror all textOverImage","divname":"Halloween-Horror-Nights", "divdatatext":"Halloween-Horror-Nights bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img18","imagepath":"Movie_images/All/Horror/Halloween-Horror-Nights.jpg"},{"divid":"Sci-fi","divclass":"character box sci-fi all textOverImage","divname":"MEG", "divdatatext":"MEG bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img20","imagepath":"Movie_images/All/Sci-Fi/MEG.jpg"},{"divid":"Sci-fi","divclass":"character box sci-fi all textOverImage","divname":"Paramount_ProjectAlmanac", "divdatatext":"Paramount_ProjectAlmanac bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img21","imagepath":"Movie_images/All/Sci-Fi/Paramount_ProjectAlmanac.jpg"},{"divid":"Sci-fi","divclass":"character box sci-fi all textOverImage","divname":"Alien-1979", "divdatatext":"Alien-1979 bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img22","imagepath":"Movie_images/All/Sci-Fi/Alien-1979.jpg"},{"divid":"Sci-fi","divclass":"character box sci-fi all textOverImage","divname":"Genesis-Action", "divdatatext":"Genesis-Action bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img23","imagepath":"Movie_images/All/Sci-Fi/Genesis-Action.jpg"},{"divid":"Romantic","divclass":"character box romantic all textOverImage","divname":"After we collided", "divdatatext":"After we collided bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img24","imagepath":"Movie_images/All/Romantic/After we collided.jpg"},{"divid":"Romantic","divclass":"character box romantic all textOverImage","divname":"Bianca", "divdatatext":"Bianca bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img25","imagepath":"Movie_images/All/Romantic/Bianca.jpg"},{"divid":"Romantic","divclass":"character box romantic all textOverImage","divname":"2 Hearts", "divdatatext":"2 Hearts bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img26","imagepath":"Movie_images/All/Romantic/Two hearts.jpg"},{"divid":"Romantic","divclass":"character box romantic all textOverImage","divname":"Crazy about her", "divdatatext":"Crazy about her bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img27","imagepath":"Movie_images/All/Romantic/Crazy about her.jpg"},{"divid":"Romantic","divclass":"character box romantic all textOverImage","divname":"Romantic", "divdatatext":"Romantic bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img29","imagepath":"Movie_images/All/Romantic/Romantic-movies.jpeg"},{"divid":"Family","divclass":"character box family all textOverImage","divname":"Titanic", "divdatatext":"Titanic bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img30","imagepath":"Movie_images/All/Family/Titanic.jfif"},{"divid":"Kids","divclass":"character box kids all textOverImage","divname":"Aladin", "divdatatext":"Aladin bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img31","imagepath":"Movie_images/All/Kids/Aladin.jfif"},{"divid":"Family","divclass":"character box family all textOverImage","divname":"The_Boss_Baby", "divdatatext":"The_Boss_Baby bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img32","imagepath":"Movie_images/All/Family/The_Boss_Baby.jpg"},{"divid":"Kids","divclass":"character box kids all textOverImage","divname":"Harry-Potter", "divdatatext":"Harry-Potter bisubfiuebfiurbifbirbgirbgierbgir","imageclass": "genre_image","imageid":"img33","imagepath":"Movie_images/All/Kids/Harry-Potter.jpg"}]' ;
+
+
+var slideIndex = 1;
+//getimages_movies();
+showSlides(slideIndex,1);
+
+
+//=------------------------------------------------------------------------------------------------------------------------------
+
+
+// Next/previous controls
+function plusSlides(n,p) {
+  showSlides(slideIndex += n,p);
+}
+//=------------------------------------------------------------------------------------------------------------------------------
+
+
+// Thumbnail image controls
+function currentSlide(n,p) {
+  showSlides(slideIndex = n,p);
+    
+}
+//=------------------------------------------------------------------------------------------------------------------------------
+
+function showSlides(n,p) {
+  var i;
+  var str1 ="carousel_slide" + p.toString();
+    
+  var slides = document.getElementsByClassName(str1);
+    
+  var dots = document.getElementsByClassName("carousel_indicator");
+  
+    
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+    
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  
+    
+}
+//=------------------------------------------------------------------------------------------------------------------------------
+
+
+function getimages(id)
+{
+    
+var genre_all = document.getElementsByClassName("all"); 
+   
+
+    for (i = 0; i < genre_all.length; i++) {
+      genre_all[i].style.display = "none";      
+  }
+	
+	console.log(genre_all);
+
+var category = document.getElementById("genrecategory");
+	console.log(category);
+var genre = category.options[category.selectedIndex].value;
+	console.log(category.selectedIndex);
+	console.log(genre);
+var genre_img = document.getElementsByClassName(genre.toLowerCase()); 
+	
+   console.log(genre_img);
+    
+if (genre_img.length>0) 
+    {
+        document.getElementById("lbl_genre").textContent  =  "Popular in " + genre;
+    }
+else if (genre_img.length=0) 
+    {
+        document.getElementById("lbl_genre").textContent  =  "No Record found";
+    }
+ else if (genre=="Genre") 
+    {
+        document.getElementById("lbl_genre").textContent  =  "";
+    }   
+
+
+    for (i = 0; i < genre_img.length; i++) {
+      genre_img[i].style.display = "block";
+      
+  }
+}
+
+
+//=------------------------------------------------------------------------------------------------------------------------------
+//=------------------------------------------------------------------------------------------------------------------------------
+
+
+const charactersList = document.getElementById('charactersList');
+const inputsearch = document.getElementById('inputsearch');
+
+
+let userdata = JSON.parse(data);
+
+inputsearch.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+
+    const filteredCharacters = userdata.filter((character) => {
+        return (
+            
+            character.divname.toLowerCase().includes(searchString)
+            
+        );
+    });
+	
+	console.log(filteredCharacters.length);
+	console.log(filteredCharacters);
+	
+    displayCharacters(filteredCharacters);
+    
+if (filteredCharacters.length==0) 
+    {
+        document.getElementById("lbl_genre").textContent  =  "No Record found";
+    }
+ else
+    {
+        document.getElementById("lbl_genre").textContent  =  "";
+    }   
+    
+});
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `        
+            <div class="${character.divclass}" id="${character.divid}" name ="${character.divname}" data-text="${character.divdatatext}">
+                <img class ="${character.imageclass}" id="${character.imageid}" src="${character.imagepath}"></img>
+            </div>        
+        `;
+        })
+        .join('');
+     
+    charactersList.innerHTML = htmlString;
+};        
+
+const loadCharacters= displayCharacters(userdata);
+
+
+
+
+
+
+
+
+
+
+
+
